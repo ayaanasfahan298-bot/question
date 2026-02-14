0753adee9c27694649f908b19f90f549a812dbca
ASRA FAZAL <ayaanasfahan298@gmail.com>
2026-02-14 02:47:03 -0800

    Add files via upload

---
+++ question .py
@@ -0,0 +1,18 @@
+print("="*40)
+print("   QUESTION PAPER   ")
+print("="*40)
+
+print("subject:computer")
+print("Time   :30 MINUTES")
+print("Marks  :20")
+print("-"*40)
+
+print("Q1: what is computer?")
+print("Q2: what is python?")
+print("Q3: Define input and output device")
+print("Q4: what is compiler")
+print("Q5: write difference between RAM and ROM")
+
+print("-"*40)
+print("         Best of Luck          ")
+print("="*40)
